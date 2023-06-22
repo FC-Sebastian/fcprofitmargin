45d4f9be780af26e6e4b1f4d88c98277b80a7e36
FC-Sebastian <sebastian.briese@fatchip.de>
2023-06-22 11:21:18 +0200

    init

---
+++ metadata.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.1';
+
+/**
+ * Module information
+ */
+$aModule = [
+    'id'          => 'fcprofitmargin',
+    'title'       => [
+        'de' => 'Gewinnberechnung',
+        'en' => 'Profit calculation'
+    ],
+    'description' => [
+        'de' => 'Platzhalter',
+        'en' => 'placeholder'
+    ],
+    'version'     => '1.0.0',
+    'author'      => 'FC-Sebastian',
+    'extend'      => [
+    ],
+    'blocks'      => [
+    ],
+    'events'      => [
+    ],
+    'controllers' => [
+    ]
+];
